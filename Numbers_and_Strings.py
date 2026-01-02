4885ed3c90f7939ea922137852669f81eba43b97
Emir Asaf AFŞARİ <245440166+kutupayisiamarockseven@users.noreply.github.com>
2026-01-02 04:52:16 +0300

    1. Numbers and Strings

---
+++ Numbers_and_Strings.py
@@ -0,0 +1,18 @@
+#######################
+# Numbers and Strings #
+#######################
+
+print("KAAR7")
+print("9")       # "9" acts like a text here (Strings)
+print(701)       # 701 acts like a number here (Integer)
+
+#Mathematical Operation and String Concatenation
+print(700+1)     #701 (Calculator logic)
+print("700"+"1") #7001 (text+text) (Concatenation)
+
+#Checking data types using type()
+print(type("kaar7")) # Strings <class 'str'>
+print(type("9"))     # Strings <class 'str'>
+print(type(9))       # Integer <class 'int'>
+print(type(9.2))     # Float   <class 'float'>
+print(type(True))    # Boolean <class 'bool'>
