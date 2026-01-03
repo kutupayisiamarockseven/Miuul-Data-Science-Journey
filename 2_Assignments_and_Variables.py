da089abb44f3ea0e2791a4ac09a6e88579954de8
Emir Asaf AFŞARİ <245440166+kutupayisiamarockseven@users.noreply.github.com>
2026-01-03 05:19:40 +0300

    Add files via upload

---
+++ 2_Assignments_and_Variables.py
@@ -0,0 +1,20 @@
+#############################
+# Assignments and Variables #
+#############################
+
+# variable_name = value
+
+a = "kaar7"
+print(a) #Prints the string stored in 'a' (kaar7)
+
+b = 9
+print(b) #Prints the integer stored in 'b' (9)
+
+c = 10
+
+# Performing calculation with variables
+
+print(b * c) #90
+
+d = b * c
+print(d) #90
